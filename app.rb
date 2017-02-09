1b417066ea5b1ce57f62e1b66dd11d6b5b8faf38
BuildTools <unconfigured@null.spigotmc.org>
2017-02-08 17:41:33 -0700

    Fix security issue

--- app.rb
+++ app.rb
@@ -1,9 +1,11 @@
 require 'sinatra'
 require 'json'
+require 'yaml'
 
 helpers do
   def verify_signature(request_body)
-    signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['SECRET_TOKEN'], request_body)
+    token = YAML.load_file('.secret.yml')['secret']
+    signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), token, request_body)
     return halt 500, "Signatures didn't match!" unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
   end
 end
@@ -16,11 +18,10 @@ post '/payload' do
   request.body.rewind
   request_body = request.body.read
   verify_signature(request_body)
-  push = JSON.parse(request_body)
+  repo = JSON.parse(request_body)['repository']
+
+  # Now let's manage everything in a separate thread
   
-  File.open('some_file.txt', 'w') do |f|
-    f.write("JSON!!! #{push.inspect}")
-  end
 
   {}.to_json
 end
