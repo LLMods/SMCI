66cff7e55bd20b9ec06c885273869724e76abb21
BuildTools <unconfigured@null.spigotmc.org>
2017-02-09 10:53:05 -0700

    Fix web app to do some cool stuff

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ helpers do
 end
 
 get '/' do
-  '<html><body><p>Hello</p></body></html>'
+  erb :index
 end
 
 post '/payload' do
@@ -20,15 +20,37 @@ post '/payload' do
   verify_signature(request_body)
   repo = JSON.parse(request_body)['repository']
 
+  return {}.to_json if repo['name'] == 'SMCI'
+  
   # Now let's manage everything in a separate thread
   Thread.new do
+    now = `date +%F_%H_%M`
+    output = "#{repo['name']}_build_log_#{now}.txt\n"
+    
     Dir.chdir('/home/minecraft/llmods') do
-      `git clone #{repo['git_url']} >> /home/minecraft/public_html/log/thin.8080.log` unless File.directory?(repo['name'])
+      if File.directory?(repo['name'])
+        Dir.chdir(repo['name']) { output += `git pull` + "\n" }
+      else
+        output += `git clone #{repo['git_url']}` + "\n"
+      end
+      
       Dir.chdir(repo['name']) do
-        # TODO: Build and install the mod
-        # TODO: Create simple log file that shows what happened in build
+        output += `mvn clean ; mvn package` + "\n"
+        if File.directory?('target')
+          Dir.chdir('target') do
+            Dir['*.jar'].each do |jar|
+              `cp #{jar} /home/minecraft/server/plugins`
+              `/etc/init.d/minecraft command reload`
+            end
+          end
+        end
       end
     end
+
+    Dir.chdir('/home/minecraft/public_html/public')
+    `mkdir #{repo['name']}` unless File.directory?(repo['name'])
+    Dir.chdir(repo['name'])
+    File.open("#{repo['name']}_#{now}.log", 'w') { |f| f.puts output }
   end
 
   {}.to_json
